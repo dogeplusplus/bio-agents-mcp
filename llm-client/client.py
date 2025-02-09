c3ec2a9431be66d26055ecd5c190598676ee5d01
Albert Chung <albertswchung@gmail.com>
2025-02-09 10:21:38 +0000

    feat: remove old weather server.

--- llm-client/client.py
+++ llm-client/client.py
@@ -1,12 +1,12 @@
-import os
 import asyncio
-from typing import List, Tuple
-from ollama import chat
-from typing import Optional
+import os
 from contextlib import AsyncExitStack
-from dotenv import load_dotenv, find_dotenv
-from mcp.client.sse import sse_client
+from typing import List, Optional
+
+from dotenv import find_dotenv, load_dotenv
 from mcp import ClientSession
+from mcp.client.sse import sse_client
+from ollama import chat
 
 load_dotenv(find_dotenv())
 
@@ -39,7 +39,13 @@ class MCPClient:
         self.llm = llm
         self.exit_stack = AsyncExitStack()
 
+    # async def authorize(self):
+    #     response = await self.session.call_tool("authorize", {})
+    #     input(f"Authorize application: {response.content}")
+
     async def connect_to_server(self, host: str = "localhost", port: int = 8080):
+        self.server_host = host
+        self.server_port = port
         url = f"http://{host}:{port}/sse"
 
         sse_transport = await self.exit_stack.enter_async_context(sse_client(url))
@@ -61,8 +67,6 @@ class MCPClient:
         ]
 
         self.available_tools = available_tools
-        print(f"Available tools: {available_tools}")
-
 
     async def process_query(self, query: str):
         messages = [
@@ -85,13 +89,12 @@ class MCPClient:
                 tool_name = function.name
                 tool_args = function.arguments
 
-                print(f"Calling tool {tool_name} with args {tool_args}")
                 result = await self.session.call_tool(tool_name, tool_args)
                 tool_results.append({"call": tool_name, "result": result})
                 final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
-                messages.append({"role": "assistant", "content": result.content[0].text[:10000]})
+                messages.append({"role": "system", "content": result.content[0].text[:10000]})
 
-            messages.append({"role": "system", "content": "Use only the assistant outputs to create the response."})
+            messages.append({"role": "system", "content": "Use the API outputs to create the response."})
             response = self.llm.send_message(messages)
             final_text.append(response.message.content)
         else:
@@ -120,16 +123,16 @@ async def main():
     ollama_port = os.environ["OLLAMA_PORT"]
 
     llm = OllamaClient(
-        model="llama3.2:1b",
+        model="qwen2.5:14b",
         base_url=f"http://{ollama_host}:{ollama_port}"
     )
     client = MCPClient(llm)
 
-    weather_host = os.environ["WEATHER_SERVER_HOST"]
-    weather_port = os.environ["WEATHER_SERVER_PORT"]
+    mcp_host = os.environ["MCP_SERVER_HOST"]
+    mcp_port = os.environ["MCP_SERVER_PORT"]
 
     try:
-        await client.connect_to_server(weather_host, weather_port)
+        await client.connect_to_server(mcp_host, mcp_port)
         await client.chat_loop()
     finally:
         await client.cleanup()
